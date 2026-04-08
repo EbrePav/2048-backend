f861101c3e43b4698b2d2f7cda6d0757a5a1cd6f
Евгений Павлишен <y.pavlyshen@appyfurious.com>
2026-04-08 17:08:54 +0300

    Fix CORS headers aggressively

--- api/api.js
+++ api/api.js
@@ -16,10 +16,16 @@ function authenticateToken(req) {
 }
 
 export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  if (req.method === 'OPTIONS') return res.status(200).end();
+  // AGGRESSIVE CORS
+  res.setHeader('Access-Control-Allow-Credentials', 'true')
+  res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization')
+
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
 
   const { pathname } = new URL(req.url, `http://${req.headers.host}`);
   const auth = authenticateToken(req);
